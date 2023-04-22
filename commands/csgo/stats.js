5c94ec723bf93a19b13e414048a466a6e3536085
emantrigo <emantrigo@hotmail.com>
2023-04-22 15:57:55 +0100

    updated stats embed

--- commands/csgo/stats.js
+++ commands/csgo/stats.js
@@ -1,19 +1,19 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
-const { TRN } = require("../../config.json");
+const { TRN } = require('../../config.json');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("stats")
-    .setDescription("Replies with Pong!")
+    .setName('stats')
+    .setDescription('Replies with Pong!')
     .addStringOption((option) =>
       option
-        .setName("steamid")
-        .setDescription("The player steam id")
+        .setName('steamid')
+        .setDescription('The player steam id')
         .setRequired(true)
     ),
   async execute(interaction) {
-    const steamId = interaction.options.getString("steamid", true);
+    const steamId = interaction.options.getString('steamid', true);
 
     try {
       let search = await fetch(
@@ -21,10 +21,10 @@ module.exports = {
           steamId
         )}`,
         {
-          method: "GET",
+          method: 'GET',
           headers: {
-            "TRN-Api-Key": TRN,
-            Accept: "application/json",
+            'TRN-Api-Key': TRN,
+            Accept: 'application/json',
           },
         }
       );
@@ -33,22 +33,22 @@ module.exports = {
 
       console.log(search);
 
-      if (search.data.lengh === 0) return interaction.reply("No user found!");
+      if (search.data.lengh === 0) return interaction.reply('No user found!');
 
       let res = await fetch(
         `https://public-api.tracker.gg/v2/csgo/standard/profile/steam/${search.data[0].platformUserId}`,
         {
-          method: "GET",
+          method: 'GET',
           headers: {
-            "TRN-Api-Key": TRN,
-            Accept: "application/json",
+            'TRN-Api-Key': TRN,
+            Accept: 'application/json',
           },
         }
       );
 
       let result = await res.json();
 
-      console.log(result);
+      console.log(result.data.segments);
 
       if (result.errors !== undefined)
         return interaction.reply(result.errors[0].message);
@@ -60,37 +60,55 @@ module.exports = {
       let deaths = result.data.segments[0].stats.deaths.displayValue;
       let shotsAccuracy =
         result.data.segments[0].stats.shotsAccuracy.displayValue;
+      let wins = result.data.segments[0].stats.wins.displayValue;
+      let matchesPlayed =
+        result.data.segments[0].stats.matchesPlayed.displayValue;
 
       let avatar = result.data.platformInfo.avatarUrl;
 
       let embed = new EmbedBuilder()
         .addFields(
           {
-            name: "Competitive Hours",
+            name: 'Competitive Hours',
             value: compHours,
           },
           {
-            name: "K/D",
+            name: 'K/D',
             value: kd,
+            inline: true,
           },
           {
-            name: "Kills",
+            name: 'Kills',
             value: kills,
+            inline: true,
           },
           {
-            name: "Deaths",
+            name: 'Deaths',
             value: deaths,
+            inline: true,
+          },
+          {
+            name: 'Shots Accuracy',
+            value: shotsAccuracy,
+          },
+          {
+            name: 'Matches Played',
+            value: matchesPlayed,
+            inline: true,
+          },
+          {
+            name: 'Wins',
+            value: wins,
+            inline: true,
           }
         )
         .setTitle(username)
         .setThumbnail(avatar);
 
-      console.log(JSON.stringify(result.data.segments));
-
       await interaction.reply({ embeds: [embed] });
     } catch (err) {
       console.log(err);
-      await interaction.reply("An error has occurred!");
+      await interaction.reply('An error has occurred!');
     }
   },
 };
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const fs = require("node:fs");
-const path = require("node:path");
+const fs = require('node:fs');
+const path = require('node:path');
 const {
   Client,
   Collection,
   GatewayIntentBits,
   Partials,
-} = require("discord.js");
-const { token } = require("./config.json");
+} = require('discord.js');
+const { token } = require('./config.json');
 
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
@@ -26,18 +26,18 @@ const client = new Client({
 client.commands = new Collection();
 client.cooldowns = new Collection();
 
-const foldersPath = path.join(__dirname, "commands");
+const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
   const commandsPath = path.join(foldersPath, folder);
   const commandFiles = fs
     .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
+    .filter((file) => file.endsWith('.js'));
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
-    if ("data" in command && "execute" in command) {
+    if ('data' in command && 'execute' in command) {
       client.commands.set(command.data.name, command);
     } else {
       console.log(
@@ -47,10 +47,10 @@ for (const folder of commandFolders) {
   }
 }
 
-const eventsPath = path.join(__dirname, "events");
+const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs
   .readdirSync(eventsPath)
-  .filter((file) => file.endsWith(".js"));
+  .filter((file) => file.endsWith('.js'));
 
 for (const file of eventFiles) {
   const filePath = path.join(eventsPath, file);
@@ -62,10 +62,10 @@ for (const file of eventFiles) {
   }
 }
 
-client.on("messageCreate", (m) => {
+client.on('messageCreate', (m) => {
   if (m.author.bot) return;
-  if (m.content === "ping") {
-    m.reply("pong");
+  if (m.content === 'ping') {
+    m.reply('pong');
   }
 });
 
